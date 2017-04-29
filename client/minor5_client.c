9c35e946254f55e60996d2bc52685b3ff3a1d2e9
GritDrop12 <laa0173@cse05.(none)>
2017-04-28 21:33:02 -0500

    First Commit

---
+++ client/minor5_client.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+   int sockfd, portno, n;
+   struct sockaddr_in serv_addr;
+   struct hostent *server;
+
+   char buffer[256];
+
+   if(argc < 2)
+   {
+	  printf("For this program please use command line argument.\n");
+	  printf("Usage would be: ./a.out quit|get|put filename\n");
+	  return 0;
+   }
+
+   portno = 3659;
+
+   /* Create a socket point */
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+   if (sockfd < 0) {
+      perror("ERROR opening socket");
+      exit(1);
+   }
+
+   server = gethostbyname("cse06.cse.unt.edu");
+
+   if (server == NULL) {
+      fprintf(stderr,"ERROR, no such host\n");
+      exit(0);
+   }
+
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+   serv_addr.sin_port = htons(portno);
+
+   /* Now connect to the server */
+   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR connecting");
+      exit(1);
+   }
+
+   if(argv[1] == "quit")
+   {
+	  n = write(sockfd, argv[1], strlen(buffer));
+	  return 0;
+   }
+
+   if(argv[1] == "put")
+   {
+	  n = write(sockfd, argv[1], strlen(argv[1]));
+	  n = write(sockfd, argv[2], strlen(buffer));
+	  FILE *zp = fopen(argv[2],"r");
+
+	  while(1)
+	  {
+		  unsigned char ruff[256] = {0};
+		  int reads = fread(ruff,1,256,zp);
+
+		  if(reads > 0)
+		  {
+			  n = write(sockfd, ruff, reads);
+		  }
+		  if(reads < 256)
+		  {
+			  if(feof(zp))
+			  {
+				  break;
+			  }
+		  }
+	  }
+   }
+   char recbuff[256];
+   if(argv[1]=="get")
+   {
+       n = write(sockfd,argv[1],strlen(buffer));
+       n = write(sockfd, argv[2], strlen(buffer));
+       FILE *zp = fopen(argv[2],"w");
+       int bytesrec;
+       while((bytesrec = read(sockfd,recbuff,256)>0))
+       {
+           fwrite(recbuff,1,bytesrec,zp);
+       }
+   }
+
+   if (n < 0) {
+      perror("ERROR writing to socket");
+      exit(1);
+   }
+
+   /* Now read server response */
+   //bzero(buffer,256);
+   //n = read(sockfd, buffer, 255);
+
+   //if (n < 0) {
+   //   perror("ERROR reading from socket");
+   //   exit(1);
+   //}
+
+   //printf("%s\n",buffer);
+   return 0;
+}
---
+++ server/minor5_server.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+
+#include <string.h>
+
+int main( int argc, char *argv[] ) {
+   int sockfd, newsockfd, portno, clilen;
+   char buffer[256];
+   struct sockaddr_in serv_addr, cli_addr;
+   int  n;
+
+   /* First call to socket() function */
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+   if (sockfd < 0) {
+      perror("ERROR opening socket");
+      exit(1);
+   }
+
+   /* Initialize socket structure */
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   portno = 3659;
+
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_addr.s_addr = INADDR_ANY;
+   serv_addr.sin_port = htons(portno);
+
+   /* Now bind the host address using bind() call.*/
+   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR on binding");
+      exit(1);
+   }
+   printf("Waiting for connections\n");
+   /* Now start listening for the clients, here process will
+      * go in sleep mode and will wait for the incoming connection
+   */
+
+   listen(sockfd,5);
+   clilen = sizeof(cli_addr);
+
+   /* Accept actual connection from the client */
+   newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+
+   if (newsockfd < 0) {
+      perror("ERROR on accept");
+      exit(1);
+   }
+
+   /* If connection is established then start communicating */
+
+   n = read( newsockfd,buffer,255 );
+   printf("%s\n",buffer);
+   if(buffer == "quit")
+   {
+       return 0;
+   }
+   else if(buffer == "put")
+   {
+       n = read(newsockfd,buffer,256);
+       FILE *xp;
+       xp = fopen(buffer,"w");
+       int bytesrec;
+       char recbuff[256];
+       while((bytesrec = read(newsockfd,recbuff,256))>0)
+       {
+           fwrite(recbuff,1,bytesrec,xp);
+       }
+
+   }
+   else if(buffer == "get")
+   {
+       n = read(newsockfd,buffer,256);
+       FILE *xp;
+       xp = fopen(buffer,"r");
+
+       while(1)
+       {
+           unsigned char rruf[256] = {0};
+           int cre = fread(rruf,1,256,xp);
+
+           if( cre > 0)
+           {
+               write(newsockfd,rruf,cre);
+           }
+
+           if( cre < 256)
+           {
+               if(feof(xp))
+               {
+                   break;
+               }
+           }
+       }
+   }
+   else
+   {
+       return 0;
+   }
+   //n = write(newsockfd,"I got your message",18);
+
+   /*if (n < 0) {
+      perror("ERROR writing to socket");
+      exit(1);
+   }*/
+
+   return 0;
+}
